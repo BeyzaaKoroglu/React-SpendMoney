2a790c074db8bcfd005ad28fa8b1d60b5fd346aa
Beyza Köroğlu <83373912+BeyzaaKoroglu@users.noreply.github.com>
2022-09-14 22:17:06 +0300

    fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ import "../styles/header.css";
 function Header() {
   return (
     <div id="header">
-      <img src={`/images/BillGates.avif`} alt="Bill Gates" />
+      <img
+        src="https://hai.stanford.edu/sites/default/files/styles/person_big/public/2020-03/billgatesheadshot.jpg?itok=6fmYlCKg"
+        alt="Bill Gates"
+      />
       <h1>Spend Bill Gates' Money</h1>
     </div>
   );
